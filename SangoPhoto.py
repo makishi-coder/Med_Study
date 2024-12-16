79896d78d066de98b15e07f363450255b845b35a
RM <e224206@eve.u-ryukyu.ac.jp>
2024-12-16 12:45:07 +0900

    20241216

--- SangoPhoto.py
+++ SangoPhoto.py
@@ -4,6 +4,7 @@ from PIL import Image
 from PIL.ExifTags import TAGS
 from datetime import datetime
 from streamlit_image_select import image_select
+from st_clickable_images import clickable_images
 
 # Supportive Aid for Needs in Global Operations by Photo management system
 st.title("SangoPhoto")
@@ -13,37 +14,62 @@ if st.button("診療番号取得"):
 
 medical_record_no = st.text_input("診療番号を入力してください", value="00000001")
 
-if st.button("写真一覧表示"):
-    st.text("test2")
+# Pythonファイルがあるディレクトリを取得
+current_directory = os.path.dirname(os.path.abspath(__file__))
+picture_directory = os.path.join(current_directory, 'assets', 'Picture')
+if not os.path.exists(picture_directory):
+    os.makedirs(picture_directory)
 
-    # Pythonファイルがあるディレクトリを取得
-    current_directory = os.path.dirname(os.path.abspath(__file__))
-    picture_directory = os.path.join(current_directory, 'assets', 'Picture')
+# 診療番号フォルダのパスを取得
+MRNo_directory = os.path.join(picture_directory, medical_record_no)
+if not os.path.exists(MRNo_directory):
+    os.makedirs(MRNo_directory)
 
-    if not os.path.exists(picture_directory):
-        os.makedirs(picture_directory)
 
-    # 診療番号フォルダのパスを取得
-    MRNo_directory = os.path.join(picture_directory, medical_record_no)
+# セッション状態に画像が保存されていなければ、アップロードを許可
+if 'image' not in st.session_state:
+    st.session_state.image = None
+
+# ファイルアップロード
+uploaded_file = st.file_uploader("カメラで写真を撮影またはアップロードしてください", type=["jpg", "jpeg", "png"])
+
+if uploaded_file is not None:
+    st.session_state.image = Image.open(uploaded_file)
     
+    # ユニークなファイル名を作成
+    timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+    unique_filename = f"{timestamp}_{uploaded_file.name}"
+    file_path = os.path.join(MRNo_directory, unique_filename)
+
+    # 画像を保存
+# アップロードされた画像を表示
+if st.session_state.image is not None:
+    
+    with Image.open(uploaded_file) as image:
+        image.save(file_path)
+    print("AAAA image_saved")
+    st.success(f"Image saved to {file_path}")
+    # st.image(file_path, caption="Uploaded Image", use_column_width=True)
+
+if st.button("写真一覧表示"):
     if not os.path.exists(MRNo_directory):
         os.makedirs(MRNo_directory)
 
     def get_image_date(image_path):
         try:
-            img = Image.open(image_path)
-            exif_data = img._getexif()
-            if exif_data is not None:
-                for tag, value in exif_data.items():
-                    if TAGS.get(tag) == 'DateTimeOriginal':
-                        return datetime.strptime(value, "%Y:%m:%d %H:%M:%S").date()
+            with Image.open(image_path) as img:
+                exif_data = img._getexif()
+                if exif_data is not None:
+                    for tag, value in exif_data.items():
+                        if TAGS.get(tag) == 'DateTimeOriginal':
+                            return datetime.strptime(value, "%Y:%m:%d %H:%M:%S").date()
         except Exception as e:
             print(f"Error processing {image_path}: {e}")
         creation_time = os.path.getctime(image_path)
         return datetime.fromtimestamp(creation_time).date()
 
     # 画像ファイル名を取得
-    image_files = [f for f in os.listdir(MRNo_directory) if f.endswith(('.png', '.jpg', '.jpeg'))]
+    image_files = [f for f in os.listdir(MRNo_directory) if f.endswith((".png", ".jpg", ".jpeg"))]
 
     # 撮影日別に画像を分類
     date_groups = {}
@@ -58,14 +84,13 @@ if st.button("写真一覧表示"):
     # 撮影日別に画像を表示
     for date_taken, item_images in sorted(date_groups.items(), reverse=True):
         st.subheader(f"日付: {date_taken}")
-        
+
         # 画面幅
         container_width = st.container().width
         cols = st.columns(5)
 
         for i, image_path in enumerate(item_images):
-            #with cols[i % 5]:
-                image = Image.open(image_path)
-                # 画像を表示（縦横比を保持）し、最大幅を指定
-                st.image(image, use_container_width=True)
-                
+            with cols[i % 5]:
+                with Image.open(image_path) as image:
+                    # 画像を表示（縦横比を保持）し、最大幅を指定
+                    st.image(image, use_container_width=True)
